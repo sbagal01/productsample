f8b7408458a9e6ccde26e2cc55a79f25d6f02147
sbagal01 <91078228+sbagal01@users.noreply.github.com>
2024-08-29 00:47:03 +0530

    Update ProductSlice.js findItems

--- src/store/ProductSlice.js
+++ src/store/ProductSlice.js
@@ -66,7 +66,7 @@ const productSlice=createSlice({
             state.product= filteredProduct;
             state.status='completed';
         },addProduct:(state,action)=>{
-            return 25;
+            return 26;
         }
     },
     extraReducers:(builder)=>{
